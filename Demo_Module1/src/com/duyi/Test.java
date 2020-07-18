44d2677248231c4d5f64c2eda319167bba6bc904
Jacky-JJ <68452525+Jacky-JJ@users.noreply.github.com>
2020-07-18 10:52:30 +0800

    Second trial

--- Demo_Module1/src/com/duyi/Test.java
+++ Demo_Module1/src/com/duyi/Test.java
@@ -3,5 +3,7 @@ package com.duyi;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world!!");
+        String s = "goodbye";
+        System.out.println(s);
     }
 }
